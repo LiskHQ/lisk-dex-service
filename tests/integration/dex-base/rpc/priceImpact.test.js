1e5671ec1853176b0e7fe77fa9b10a392841f671
Irfan Mustafa <irfanmustafa31201@gmail.com>
2023-04-05 02:29:12 -0400

    added method for getting priceImpact and the integration test

---
+++ tests/integration/dex-base/rpc/priceImpact.test.js
@@ -0,0 +1,35 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const config = require('../../../config');
+const { request } = require('../../../helpers/socketIoRpcRequest');
+
+
+const wsRpcUrl = `${config.SERVICE_ENDPOINT}/rpc-dex-v1`;
+const getPriceImpact = async params => request(wsRpcUrl, 'get.prices.impact', params);
+
+describe('Method get.prices.impact', () => {
+    describe('is able toReturn the impact price on a swap trade on the market price of the pool.', () => {
+        it('returns impact price', async () => {
+            try {
+                const response = await getPriceImpact({ poolID: '0000000000000000000001000000000000c8', tokenAmount0: '10', tokenAmount1: '5', isZeroToOne: false });
+                const { result } = response;
+                expect(result.priceImpact).not.toBe('0.0000');
+            } catch (err) {
+                throw err;
+            }
+        });
+    });
+});

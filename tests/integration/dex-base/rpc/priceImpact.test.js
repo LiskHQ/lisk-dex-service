126c7c7cbf53aaca1f0b019b33ffbc8cc8670fa8
Irfan Mustafa <irfanmustafa31201@gmail.com>
2023-04-25 03:09:33 -0400

    removed a deepScan issue

--- tests/integration/dex-base/rpc/priceImpact.test.js
+++ tests/integration/dex-base/rpc/priceImpact.test.js
@@ -24,10 +24,20 @@ describe('Method get.prices.impact', () => {
     describe('is able toReturn the impact price on a swap trade on the market price of the pool.', () => {
         it('returns impact price', async () => {
             try {
-                const response = await getPriceImpact({ poolID: '0000000000000000000001000000000000c8', tokenAmount0: '10', tokenAmount1: '5', isZeroToOne: false });
+                const moduleEndpointContext = {
+                    params:{
+                        tokenIdIn: Buffer.from('0000000000000000', 'hex'),
+                        amountIn:50,
+                        tokenIdOut:Buffer.from('0000010000000000', 'hex'),
+                        minAmountOut:10,
+                        swapRoute:[Buffer.from('0000000000000000000001000000000000c8', 'hex')],
+                    }
+                }                
+                const response = await getPriceImpact(moduleEndpointContext);
                 const { result } = response;
                 expect(result.priceImpact).not.toBe('0.0000');
             } catch (err) {
+                //TODO: Modify the error throwing logic
                 throw err;
             }
         });
